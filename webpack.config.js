0d11e1a65f0c90736f2924a7cb37e779d9f14399
erzinap <ErzinAP@kamaz.ru>
2023-10-30 18:04:12 +0300

    geo-services  without ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+let mode = 'development';
+
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+}
+
+const plugins = [
+    new HTMLWebpackPlugin({
+        template: './src/index.html/',
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+    }),
+];
+
+if (process.env.SERVE) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+}
 
 module.exports = {
-    plugin: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html'),
-            filename: 'index.html',
-        }),
-    ],
-};
+    mode,
+    plugins,
+    entry: './src/main.jsx',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    devServer: {
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            { test: /\.(html)$/, use: ['html-loader'] },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    }
+}
